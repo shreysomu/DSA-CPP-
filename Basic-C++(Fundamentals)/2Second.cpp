acb5d8655d22fe8536f58bc3d28642e5618f7b24
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2024-02-03 23:15:30 +0530

    Add files via upload

---
+++ Basic-C++(Fundamentals)/2Second.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int number;
+  cout << "enter a Number :";
+  cin >> number;
+  cout << "You Have entered :" << number << endl;
+  return 0;
+}
