29ee05b8e58d6b0ed93ef19ab23d3efa0e6e1f42
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2024-02-05 12:23:28 +0530

    Add files via upload

---
+++ Patterns/25InvertedFullPyramid.cpp
@@ -0,0 +1,34 @@
+//  * * * * * * * * * *
+//  * * * * * * * * *
+//   * * * * * * * *
+//    * * * * * * *
+//     * * * * * *
+//      * * * * *
+//       * * * *
+//        * * *
+//         * *
+//          *
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << " Enter the number of rows : ";
+  cin >> n;
+
+  for (int row = 0; row < n; row++)
+  {
+    for (int col = 0; col < row; col++)
+    {
+      cout << " ";
+    }
+
+    for (int col = 0; col < n - row; col++)
+    {
+      cout << "* ";
+    }
+    cout << endl;
+  }
+  return 0;
+}
