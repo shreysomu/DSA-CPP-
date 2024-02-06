35e18c380f61381e874b55777a04f19999efc33c
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2024-02-06 23:01:49 +0530

    Add files via upload

---
+++ Patterns/32NumericHalfPyramid.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int n;
+  cout << "Enter the number of Rows :";
+  cin >> n;
+  for (int row = 0; row < n; row++)
+  {
+    
+    for (int col = 0; col < row + 1; col++)
+    {
+      cout << col + 1;
+    }
+    cout << endl;
+  }
+  return 0;
+}
