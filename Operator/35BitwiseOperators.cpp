a10e88d6c6ea911fc05ce562b773370754e8ed44
Shrey Trivedi <54596226+shreysomu@users.noreply.github.com>
2024-02-07 22:35:35 +0530

    Add files via upload

---
+++ Operator/35BitwiseOperators.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+  bool a = true;
+  bool b = false;
+  cout<<(a&b)<<endl;
+  cout<<(a|b)<<endl;
+  cout<<~(a&b)<<endl;
+  cout<<(a^b)<<endl;
+}
